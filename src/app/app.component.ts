514972aebd5f3e65a30b74a6d6f294ffaec4dc9c
proactivecoder91 <proactivecoder91@gmail.com>
2023-11-14 16:15:37 +0530

    feat: header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
