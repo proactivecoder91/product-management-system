89c1bed046afbcfa773c38fa11ef1c64c0a391ed
proactivecoder91 <proactivecoder91@gmail.com>
2023-11-14 15:03:14 +0530

    feat: components added

---
+++ src/app/components/add-product/add-product.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-product.component.html',
+  styleUrl: './add-product.component.css'
+})
+export class AddProductComponent {
+
+}
