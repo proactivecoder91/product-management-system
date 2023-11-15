cff38fd81547375ae51c96cf26dbfffa8b7a65f2
proactivecoder91 <proactivecoder91@gmail.com>
2023-11-15 17:44:05 +0530

    feat: added a basic form layout with angular material components like mat-button , mat-card , mat-input, mat-form-filed; Pending with adding few other parameters and handling the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FormsModule} from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent,HeaderComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent,HeaderComponent, FormsModule , ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule,MatInputModule,MatFormFieldModule,MatCardModule,MatButtonModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
 export class AddProductComponent {
-
+  addProductForm!: FormGroup;
+constructor(private fb: FormBuilder){
+  this.addProductForm = this.fb.group({
+      productName: ['', Validators.required],
+      price: ['', Validators.required],
+      category: [''],
+      brand: [''],
+      quantityInStock: [''],
+      supplier: [''],
+      image: [''],
+      description: [''],
+      weight: [''],
+      dimensions: [''],
+      availabilityStatus: ['']
+  })
+}
 }
