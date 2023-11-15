eae814fda1e91ee8096cb1108b39e8c2f356c596
proactivecoder91 <proactivecoder91@gmail.com>
2023-11-15 19:29:14 +0530

    feat: added a mat-select dropdown and the new control flow syntax for the for loop

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -4,17 +4,19 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule,MatInputModule,MatFormFieldModule,MatCardModule,MatButtonModule],
+  imports: [CommonModule,ReactiveFormsModule,MatInputModule,MatFormFieldModule,MatCardModule,MatButtonModule,MatSelectModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
 export class AddProductComponent {
   addProductForm!: FormGroup;
+  productCategories:string[] = ['Electronics','Food','Cosmetics']
 constructor(private fb: FormBuilder){
   this.addProductForm = this.fb.group({
       productName: ['', Validators.required],
@@ -23,10 +25,10 @@ constructor(private fb: FormBuilder){
       brand: [''],
       quantityInStock: [''],
       supplier: [''],
-      image: [''],
       description: [''],
       weight: [''],
       dimensions: [''],
+      manufacturingDate:[''],
       availabilityStatus: ['']
   })
 }
