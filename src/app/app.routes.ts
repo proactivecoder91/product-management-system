9939b04d15b041f376209f7b3f800ad533427bb8
proactivecoder91 <proactivecoder91@gmail.com>
2023-11-20 14:50:58 +0530

    feat: configured a route for productList component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 export const routes: Routes = [
-    {path:'', component:AddProductComponent}
+    {path:'', component:AddProductComponent},
+    {path:'productList', component:ProductListComponent}
 ];
