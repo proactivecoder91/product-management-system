73ef669f3fd74f2e0fd8afeb05087f5ccc7ec3fa
proactivecoder91 <proactivecoder91@gmail.com>
2023-11-25 17:13:53 +0530

    feat: Download the product details in an excel file

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -7,6 +7,8 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {Router} from '@angular/router'
+import * as XLSX from 'xlsx';
+
 
 @Component({
   selector: 'app-product-details',
@@ -23,6 +25,22 @@ ngOnInit(){
   this.id = this.route.snapshot.params['id'];
   this.productService.getProductDetail(this.id).subscribe(data => this.productDetail = data,(error)=> console.error(error))
 }
+
+downloadExcel() {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet([this.productDetail]);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const {id} = this.productDetail 
+    this.saveAsExcelFile(excelBuffer, `product_${id}`);
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8' });
+    const downloadLink: HTMLAnchorElement = document.createElement('a');
+    downloadLink.href = window.URL.createObjectURL(data);
+    downloadLink.download = `${fileName}.xlsx`;
+    downloadLink.click();
+  }
 
 navigateBack(){
 this.router.navigate(['/productList'])
